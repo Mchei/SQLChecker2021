26647051735ab4db85e8d298ac50a0bbf7584c4c
Kwun Fung TSANG <tsaky014@mymail.unisa.edu.au>
2021-05-20 22:39:31 +0930

    update

--- SQLChecker2021/Form1.cs
+++ SQLChecker2021/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Reflection;
 
 namespace SQLChecker2021
 {
     public partial class login : Form
     {
+
+
         public login()
         {
             InitializeComponent();
@@ -26,17 +29,23 @@ namespace SQLChecker2021
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string startupPath = System.IO.Directory.GetCurrentDirectory();
+            string startupPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             string connectionString;
             SqlConnection conn;
             connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + startupPath + "\\SQLProjectDB.mdf;Integrated Security=True";
+            connectionString = connectionString.Replace("\\bin\\Debug", "");
+            Console.WriteLine(connectionString);
+            //\bin\Debug
             //connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dilig\source\repos\SQLChecker2021\SQLChecker2021\SQLProjectDB.mdf;Integrated Security=True";
             conn = new SqlConnection(connectionString);
             conn.Open();
-            MessageBox.Show("Connection Open");
+            //MessageBox.Show("Connection Open " + connectionString);
             conn.Close();
 
+            new form1().Show();
+            this.Hide();
 
+            /*
             if (txtUserName.Text == "demo" && txtpassword.Text == "1234")
             {
                 new form1().Show();
@@ -51,6 +60,7 @@ namespace SQLChecker2021
                 txtpassword.Clear();
                 txtUserName.Focus();
             }
+            */
         }
 
         private void label2_Click(object sender, EventArgs e)
