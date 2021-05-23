96df19cb84a904c03aa7d19fa2d92906c5459646
Michael <mc.hei@outlook.com>
2021-05-23 18:31:43 +0930

    Updated again

--- SQLChecker2021/Program.cs
+++ SQLChecker2021/Program.cs
@@ -17,6 +17,7 @@ namespace SQLChecker2021
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new login());
+            //Application.Run(new form1());
         }
     }
 }
